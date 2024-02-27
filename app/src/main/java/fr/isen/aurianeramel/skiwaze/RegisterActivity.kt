013688ac80e12a87304746e26a87e1a204aaade9
aurirml <auriane.ramel@orange.fr>
2024-02-27 15:01:30 +0100

    personnalisation page connexion

--- app/src/main/java/fr/isen/aurianeramel/skiwaze/RegisterActivity.kt
+++ app/src/main/java/fr/isen/aurianeramel/skiwaze/RegisterActivity.kt
@@ -6,7 +6,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +25,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -29,7 +33,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
@@ -49,6 +57,7 @@ class RegisterActivity : ComponentActivity() {
         }
 
         setContent {
+            Background()
             val mail = remember { mutableStateOf(TextFieldValue("")) }
             val password = remember { mutableStateOf(TextFieldValue("")) }
 
@@ -70,20 +79,18 @@ class RegisterActivity : ComponentActivity() {
                     visualTransformation = PasswordVisualTransformation(),
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
                 )
+                Spacer(Modifier.height(5.dp))
                 Button(
                     onClick = { addUser(mail.value.text, password.value.text) }
-                ){
+                ) {
                     Text("Créer un compte")
                 }
-                Spacer(modifier = Modifier.height(10.dp))
-                Row {
+                Spacer(Modifier.height(10.dp))
+                Row (verticalAlignment = Alignment.CenterVertically){
                     Text(
                         text = "Vous avez déjà un comte ?"
-
                     )
-                    Spacer(modifier = Modifier.width(10.dp))
                     signIn()
-
                 }
             }
         }
@@ -97,7 +104,6 @@ class RegisterActivity : ComponentActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     val user = auth.currentUser
-
                     reload()
                 } else {
                     // If sign in fails, display a message to the user.
@@ -125,7 +131,7 @@ class RegisterActivity : ComponentActivity() {
 }
 
 @Composable
-fun signIn(){
+fun signIn() {
     val context = LocalContext.current
     TextButton(
         onClick = {
