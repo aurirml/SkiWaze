f331f9e7110f8a0caa8ab298b65ef1ff4fc16f2a
aurirml <auriane.ramel@orange.fr>
2024-04-04 16:36:20 +0200

    design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,8 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.compose.material:material:1.6.4")
+
 
 
 }
